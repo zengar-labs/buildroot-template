0730dc9d76d43e4cb356be6596868e7178b85e2d
Salvador Z <32500615+salvadorz@users.noreply.github.com>
2023-06-30 18:10:44 -0500

    Add an EXTERNAL custom package from git (#2)

--- base_external/external.mk
+++ base_external/external.mk
@@ -1,2 +1,2 @@
 # Include any mk files in the package directory
-#include $(sort $(wildcard $(BR2_EXTERNAL_project_base_PATH)/package/*/*.mk))
+include $(sort $(wildcard $(BR2_EXTERNAL_project_base_PATH)/package/*/*.mk))
